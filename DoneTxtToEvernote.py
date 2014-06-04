f46a9f6e2993131a398ee4e6e23781be34ee5726
Charles Baker <charles.baker@infor.com>
2014-06-04 17:39:10 -0400

    Finally working syntax to invoke Geeknote w/ args

--- DoneTxtToEvernote.py
+++ DoneTxtToEvernote.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python
 
 import time
+import subprocess
 
 DONE = '/Users/chbaker/Dropbox/todo/done.txt'
 GEEKNOTE = '/usr/local/bin/geeknote'
 PREFIX = 'x '
+NOTEBOOK = 'Done'
 
 today = time.strftime("%Y-%m-%d")
 
@@ -17,9 +19,10 @@ body = ""
 with open(DONE, 'r') as done_file:
     for line in done_file:
         if line.startswith(search_string):
-            body += line[2:].strip()
+            body += line[2:].rstrip()
             count += 1
 
-# Debug
-#print "{} tasks completed {}".format(count, today)
-title = "{} tasks completed {}".format(count, today)
+ARGS = ' create --title "' + str(count) + ' tasks completed ' + today +
+  '" --content "' + body + '" --notebook ' + NOTEBOOK
+
+subprocess.call(GEEKNOTE + ARGS, shell = True)
