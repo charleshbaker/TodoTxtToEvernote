e303268276dcb0a2658c9a339b2da5583085f6a2
charleshbaker <charleshbaker@gmail>
2014-06-05 17:28:08 -0400

    Added some comments. Some beautifying. Changed to use crunchbang /usr/bin/env python

--- DoneTxtToEvernote.py
+++ DoneTxtToEvernote.py
@@ -1,25 +1,26 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 '''
 Parse a done.txt file in the format of Gina Trapani's todo.txt application for
 items done $TODAY. Concatenate those items in a string and use Geeknote to
 create a note in Evernote with those items as the body.
 '''
 
+# TODO Accept command line arguments but set reasonable defaults
+# TODO Create a usage function
+
 # Import python libs
 import time
 import subprocess
 
+# Constants
 DONE = '/Users/chbaker/Dropbox/todo/done.txt'
 GEEKNOTE = '/usr/local/bin/geeknote'
 PREFIX = 'x '
 NOTEBOOK = 'Done'
 
 today = time.strftime('%Y-%m-%d')
-
 search_string = PREFIX + today
-
 count = 0
-
 body = ''
 
 with open(DONE, 'r') as done_file:
