ba413ba94dde530463e4c17f1b82abe9ef1e4a97
charleshbaker <charleshbaker@gmail>
2014-06-06 15:22:47 -0400

    Added a debug function. Added CLI option parsing.

--- DoneTxtToEvernote.py
+++ DoneTxtToEvernote.py
@@ -5,31 +5,78 @@ items done $TODAY. Concatenate those items in a string and use Geeknote to
 create a note in Evernote with those items as the body.
 '''
 
-# TODO Accept command line arguments but set reasonable defaults
-# TODO Create a usage function
 
 # Import python libs
-import time
+from optparse import OptionParser
 import subprocess
+import time
 
-# Constants
-DONE = '/Users/chbaker/Dropbox/todo/done.txt'
-GEEKNOTE = '/usr/local/bin/geeknote'
+def debug(mystring):
+    print mystring
+    
+debugOn = True
+
+usage = 'usage: %prog [options]'
+    
+parser = OptionParser()
+
+parser.set_defaults(done='/Users/chbaker/Dropbox/todo/done.txt')
+parser.set_defaults(geeknote='/usr/local/bin/geeknote')
+parser.set_defaults(notebook='DONE')
+
+parser.add_option('-f',
+                '--file',
+                help='Path to done.txt',
+                action='store',
+                type='string',
+                dest='done',
+                metavar='DONE'
+                    )
+                    
+parser.add_option('-g',
+                '--geeknote',
+                help='Path to geeknote',
+                action='store',
+                type='string',
+                dest='geeknote',
+                metavar='GEEKNOTE'
+                    )
+                    
+parser.add_option('-n',
+                '--notebook',
+                help='Destination notebook',
+                action='store',
+                type='string',
+                dest='notebook',
+                metavar='NOTEBOOK'
+                    )
+                    
+(options, args) = parser.parse_args()
+
+if (debugOn):
+    debug(options.done)
+    debug(options.geeknote)
+    debug(options.notebook)    
+
+# Constant
 PREFIX = 'x '
-NOTEBOOK = 'Done'
 
 today = time.strftime('%Y-%m-%d')
-search_string = PREFIX + today
+searchString = PREFIX + today
 count = 0
 body = ''
 
-with open(DONE, 'r') as done_file:
-    for line in done_file:
-        if line.startswith(search_string):
-            body += line[13:].rstrip() + '\n'
+with open(options.done, 'r') as doneFile:
+    for line in doneFile:
+        if line.startswith(searchString):
+            body += line[13:]
             count += 1
 
-ARGS = ' create --title "' + str(count) + ' tasks completed ' + today + \
-  '" --content "' + body + '" --notebook ' + NOTEBOOK
+geeknoteArgs = ' create --title "' + str(count) + ' tasks completed ' + today + \
+  '" --content "' + body + '" --notebook "' + options.notebook + '"'
 
-subprocess.call(GEEKNOTE + ARGS, shell = True)
+if (debugOn):
+    debug(geeknoteArgs)
+
+if (!debugOn):    
+    subprocess.call(GEEKNOTE + ARGS, shell = True)
